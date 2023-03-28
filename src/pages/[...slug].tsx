46e3633a3f59d2be68b26c65f886ed049bdd714c
remorses <beats.by.morse@gmail.com>
2023-03-28 15:53:54 +0200

    pages/[...slug]: edge

--- src/pages/[...slug].tsx
+++ src/pages/[...slug].tsx
@@ -1,10 +1,4 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
-import styles from '@/styles/Home.module.css'
 import { GetServerSidePropsContext } from 'next'
-
-const inter = Inter({ subsets: ['latin'] })
 
 export default function Home({ slug }) {
     return (
@@ -13,6 +7,8 @@ export default function Home({ slug }) {
         </>
     )
 }
+
+export const config = { runtime: 'experimental-edge' }
 
 export function getServerSideProps(context: GetServerSidePropsContext) {
     let slug = JSON.stringify(context.params?.slug || 'no slug')
